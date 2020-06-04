5e3c62c1342c95c51fc4537df2c4f19174ba4970
JuanYuco <37229257+JuanYuco@users.noreply.github.com>
2020-06-04 14:36:26 -0500

    Versión Final

---
+++ ProyectoFinalClases.Logica/BL/Activities.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ProyectoFinalClases.Logica.BL
+{
+    public class Activities
+    {
+        public List<Models.DB.Activities> GetActivities()
+        {
+            DAL.Models.ProyectoFinalClasesContext _context = new DAL.Models.ProyectoFinalClasesContext();
+
+            var listActivities = (from _activities in _context.Activities
+                                  where _activities.Active == true
+                                  select new Models.DB.Activities
+                                  {
+                                      Id = _activities.Id,
+                                      Name = _activities.Name,
+                                      Active = _activities.Active
+                                  }).ToList();
+
+            return listActivities;
+        }
+    }
+}
---
+++ ProyectoFinalClases.Logica/BL/Priorities.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ProyectoFinalClases.Logica.BL
+{
+    public class Priorities
+    {
+        public List<Models.DB.Priorities> GetPriorities()
+        {
+            DAL.Models.ProyectoFinalClasesContext _context = new DAL.Models.ProyectoFinalClasesContext();
+
+            var listPriorities = (from _priorities in _context.Priorities
+                                  where _priorities.Active == true
+                                  select new Models.DB.Priorities
+                                  {
+                                      Id = _priorities.Id,
+                                      Name = _priorities.Name,
+                                      Active = _priorities.Active
+                                  }).ToList();
+
+            return listPriorities;
+        }
+    }
+}
